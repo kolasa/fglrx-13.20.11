edf23ae8816486f1978f712a0a280320ff493769
Krzysztof Kolasa <kkolasa@winsoft.pl>
2013-10-01 20:30:28 +0200

    Add string GPL0 for kernel 3.12.0-rc3

--- firegl_public.c
+++ firegl_public.c
@@ -259,7 +259,11 @@ module_param(firegl, charp, 0);
 #endif
 
 #ifdef MODULE_LICENSE
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,12,0)
 MODULE_LICENSE("Proprietary. (C) 2002 - ATI Technologies, Starnberg, GERMANY");
+#else
+MODULE_LICENSE("GPL\0Proprietary. (C) 2002 - ATI Technologies, Starnberg, GERMANY");
+#endif
 #endif
 #ifdef MODULE_DEVICE_TABLE
 MODULE_DEVICE_TABLE(pci, fglrx_pci_table);
